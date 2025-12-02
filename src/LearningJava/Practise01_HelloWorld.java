56277c357d2658f8b94a3c235dff12c6a3cafc8d
Santhosh Varma Potturi <varma2105@gmail.com>
2025-12-02 14:15:46 +0530

    Initial commit

---
+++ src/LearningJava/Practise01_HelloWorld.java
@@ -0,0 +1,16 @@
+package LearningJava;
+
+public class Practise01_HelloWorld {
+    static void main() {
+        System.out.printf("Helloworld");
+
+
+    }
+    private  static void display() {
+        System.out.printf("Hello and welcome!");
+    }
+
+    }
+
+
+
---
+++ src/LearningJava/Practise02_Variables.java
@@ -0,0 +1,20 @@
+package LearningJava;
+
+public class Practise02_Variables {
+    static void main() {
+        int a =10;
+        System.out.printf("value of a is %d",a);
+        int age =19;
+         age =19+1;
+        System.out.printf("value of age is %d",age);
+        int _= 19;
+//variable can start with underscore
+        int _12=123;
+        // variable can start with $
+        int $=1234;
+// variable can have a data type boolean
+        boolean abc= true;
+        System.out.println("Hello");
+        
+    }
+}
